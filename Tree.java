077cae7387daf2f13fe657a00e3618110dfe2daf
Ryuleh <92819040+Ryuleh@users.noreply.github.com>
2024-10-09 07:44:55 -0400

    Update Tree.java

--- Tree.java
+++ Tree.java
@@ -1,20 +1,21 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
 class Tree {
-    private Node root;
+   private Node root;
 
-    public Tree(String rootName) {
-        root = new Node(rootName, true);
-    }
+   public Tree(String var1) {
+      this.root = new Node(var1, true);
+   }
 
-    public Node getRoot() {
-        return root;
-    }
+   public Node getRoot() {
+      return this.root;
+   }
 
-    public void addDirectory(Node parent, String directoryName) {
-        Node newDirectory = new Node(directoryName, true);
-        parent.addChild(newDirectory);
-    }
+   public void addDirectory(Node var1, String var2) {
+      Node var3 = new Node(var2, true);
+      var1.addChild(var3);
+   }
 
-    public void removeDirectory(Node parent, Node directory) {
-        parent.removeChild(directory);
-    }
+   public void removeDirectory(Node var1, Node var2) {
+      var1.removeChild(var2);
+   }
 }
